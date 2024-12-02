f84dced132677fcfd78de265005a37504eb88968
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2024-12-02 02:10:38 -0300

    Correção de bug ao buscar

--- AppMinhasAnotacoes/src/services/noteSearch.js
+++ AppMinhasAnotacoes/src/services/noteSearch.js
@@ -1,17 +1,28 @@
-// src/api/api.js
-import axios from "axios";
 import { API_URL } from '@env';
-
 
 // Função para buscar as notas da API
 export const SearchNotes = async (query) => {
-    
-    try {
-        const response = await axios.get(`${API_URL}/${encodeURIComponent(query)}`);
-        console.log(response.data)
-        return response.data;
+  const encodedQuery = encodeURIComponent(query);
 
-    } catch (error) {
-        throw new Error("Erro ao buscar notas: " + error);
+  try {
+
+    const response = await fetch(`${API_URL}${encodedQuery}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Erro ao buscar notas: ${response.statusText}`);
     }
+
+    const data = await response.json();
+    console.log("Dados da resposta:", data);
+    return data;
+
+  } catch (error) {
+    console.error("Erro ao buscar notas:", error); // Trata erro
+    throw new Error("Erro ao buscar notas: " + error);
+  }
 };
